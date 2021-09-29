af7057a9846fdd2ac1de1d0eac3ea4320579a9a7
Matthew Ballance <matt.ballance@gmail.com>
2021-09-29 14:08:09 -0700

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/hvl/tblink_rpc.sv
+++ src/hvl/tblink_rpc.sv
@@ -643,6 +643,46 @@ package tblink_rpc;
 		endfunction
 `endif /* UNDEFINED */
 	endclass
+	
+	class IEndpointServices;
+		
+		virtual function void init(IEndpoint ep);
+		endfunction
+		
+		// TODO: args()
+		
+		virtual function void shutdown();
+		endfunction
+		
+		virtual function int add_time_cb(
+			longint unsigned			simtime,
+			longint						callback_id);
+		endfunction
+		
+		virtual function void cancel_callback(
+			longint						callback_id);
+		endfunction
+		
+		virtual function longint unsigned get_time();
+		endfunction
+		
+		virtual function int time_precision();
+		endfunction
+		
+		virtual function void run_until_event();
+		endfunction
+		
+		virtual function void hit_event();
+		endfunction
+		
+		virtual function void idle();
+		endfunction
+		
+	endclass
+	
+	class EndpointServicesDpi extends IEndpointServices;
+	endclass
+	
 
 	/*
 	chandle _endpoint_h = null;
