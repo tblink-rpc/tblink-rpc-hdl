717d7a4422a406facd23c73267cba1acbdc59984
Matthew Ballance <matt.ballance@gmail.com>
2022-03-27 15:56:29 -0700

    Updates to remote-object example
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- tests/sv/uvm_python_remote_obj/uvm_python_remote_obj.svh
+++ tests/sv/uvm_python_remote_obj/uvm_python_remote_obj.svh
@@ -1,28 +1,22 @@
-
 /****************************************************************************
  * uvm_python_remote_seq.svh
  ****************************************************************************/
 
-typedef class uvm_python_remote_obj;
-  
 /**
  * Class: uvm_python_remote_obj
  * 
- * TODO: Add class documentation
+ * Implements the SystemVerilog view of the remote object.
+ * 
+ * There are no method implementations here because all the 
+ * calls go SV->Python in this example.
+ * 
+ * The base class (uvm_python_obj_null) is auto-generated.
  */
 class uvm_python_remote_obj extends TbLinkLaunchUvmObj #(
-		uvm_python_seq_proxy #(uvm_python_seq_null #(uvm_object))
+		uvm_python_obj_proxy #(uvm_python_obj_null #(uvm_object))
 		);
 	`uvm_object_utils(uvm_python_remote_obj)
 	
-//	typedef uvm_python_seq_proxy #(uvm_python_remote_obj) proxy_t;
-	
-	task doit(int a);
-		$display("[%0d] --> doit(%0d)", $time, a);
-		#10;
-		$display("[%0d] <-- doit(%0d)", $time, a);
-	endtask
-
 endclass
 
 
--- tests/sv/uvm_python_remote_obj/uvm_python_test.svh
+++ tests/sv/uvm_python_remote_obj/uvm_python_test.svh
@@ -10,62 +10,55 @@
 class uvm_python_test extends uvm_test;
 	`uvm_component_utils(uvm_python_test)
 	
-	TbLinkAgent					m_svagent;
-	TbLinkAgent					m_pyagent;
-
 	function new(string name, uvm_component parent=null);
 		super.new(name, parent);
 	endfunction
 	
 	function void build_phase(uvm_phase phase);
-		// Now, can build remaining components
-		/*
-		TbLinkAgentConfig sv_cfg = new("connect.sv.loopback");
-		
-		m_svagent = TbLinkAgent::type_id::create("m_svagent", this);
-		
-		if (!m_svagent.init(sv_cfg)) begin
-			`uvm_fatal(get_name(), "Failed to initialize TbLink SV Agent");
-			return;
-		end
-		 */
+		 //
 	endfunction
 	
 	function void connect_phase(uvm_phase phase);
 	endfunction
 	
+	uvm_python_remote_obj m_ref_model;
+	
+	
 	task run_phase(uvm_phase phase);
 		string python;
-		uvm_python_remote_obj obj = new();
-		/*
-		TbLinkAgentConfig cfg = new("python.loopback");
+		m_ref_model = new();
+
+		// Create the launch configuration, specifying the Python
+		// module that contains the object class
+		m_ref_model.set_config(mkConfigPythonSingleObject("uvm_python_obj"));
 		
-		cfg.launch_params["module"] = "tblink_rpc.rt.cocotb";
-		if ($value$plusargs("python=%s", python)) begin
-			cfg.launch_params["python"] = python;
-		end
-	
-		cfg.ep_args.push_back("+entry=tblink_rpc.rt.cocotb.single_inst_runner");
-		cfg.ep_args.push_back("+m=uvm_python_seq");
-		 */
-		obj.set_config(mkConfigPythonSingleObject("uvm_python_seq"));
+		// Start the remote object. If this fails, it will
+		// signal a UVM fatal error
+		m_ref_model.launch();
+	endtask
 		
 		phase.raise_objection(this, "Main", 1);
-		obj.launch();
-		
 		for (int i=0; i<5; i++) begin
 			for (int j=0; j<5; j++) begin
+				// Call the remote object
 				int val = obj.add(i, j);
 				$display("[%0t] a=%0d b=%0d: %0d", $time, i, j, val);
-				#10ns;
 			end
 		end
-		
-		$display("DONE");
-		
-		#1ms;
 		phase.drop_objection(this, "Main", 1);
 	endtask
+	
+	function void check(
+		int unsigned a, 
+		int unsigned b,
+		int unsigned result);
+		int unsigned exp = m_ref_model.add(a, b);
+		
+		if (exp != result) begin
+			`uvm_error("UvmPythonTest", $sformat("A=%0d B=%0d result=%0d ; exp=%0d",
+					a, b, result, exp));
+		end
+	endfunction
 
 
 endclass
